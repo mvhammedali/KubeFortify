7f36f415b38122e8f66b2b10a4d83452c19d42f4
Muhammed Ali <muhamzyali@gmail.com>
2023-11-19 18:55:22 +0100

    Description in README

---
+++ README.py
@@ -0,0 +1,14 @@
+# KubeFortify: Automated Resilience and Load Testing Framework for Kubernetes
+
+KubeFortify is a tool designed to ensure the resilience and performance of applications deployed on Kubernetes. It integrates load testing, resilience testing, and automated recovery processes into a single framework, leveraging AI to analyze issues and recommend solutions. KubeFortify aims to enhance system reliability and operational efficiency.
+KubeFortify is tailored for organizations that rely on Kubernetes for application deployment and require an easy to use tool to maintain service quality and uptime. Its integrated approach to testing and recovery makes it a relevant tool in the toolkit of DevOps teams and site reliability engineers.
+
+## Key Features
+1. **Load Testing**: Simulates high traffic and usage scenarios to evaluate the performance limits and scalability of Kubernetes-managed applications.
+2. **Resilience Testing**: Introduces faults into the system to assess the robustness and fault tolerance of the deployment, ensuring that the application can handle and recover from unexpected conditions.
+3. **Automated Recovery**: Monitors the deployment status post-failure and automatically attempts recovery actions if the system does not return to the desired state, ensuring minimal downtime.
+4. **AI-Driven Issue Resolution**: Utilizes OpenAI’s GPT to generate insights and practical solutions when problems are detected, enhancing the decision-making process during incident management.
+5. **Email Notifications**: Sends detailed alerts and recovery actions to system administrators, keeping the operations team informed of the system status and automated interventions.
+
+
+
