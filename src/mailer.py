63104cf22e9b91aad478c800099a47bc2e52c72a
Muhammed Ali <muhamzyali@gmail.com>
2023-12-20 19:45:21 +0100

    black formatting

--- src/mailer.py
+++ src/mailer.py
@@ -1,39 +1,41 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import config
 import os
 from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
-client = OpenAI(api_key=os.getenv('OPENAI_KEY'))
+client = OpenAI(api_key=os.getenv("OPENAI_KEY"))
 
 
 def get_solution(description):
-    response = client.completions.create(model="gpt-3.5-turbo-instruct",
-    prompt=f"Describe a problem and propose a detailed solution:\n\nProblem: {description}\n\nSolution:",
-    max_tokens=300)
+    response = client.completions.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt=f"Describe a problem and propose a detailed solution:\n\nProblem: {description}\n\nSolution:",
+        max_tokens=300,
+    )
     return response.choices[0].text.strip()
 
 
 # Send email function
 def send_email(subject, body):
     message = MIMEMultipart()
-    message['From'] = os.getenv('EMAIL_SENDER')
-    message['To'] = os.getenv('EMAIL_RECEIVER')
-    message['Subject'] = subject
+    message["From"] = os.getenv("EMAIL_SENDER")
+    message["To"] = os.getenv("EMAIL_RECEIVER")
+    message["Subject"] = subject
     message.attach(MIMEText(body, "plain"))
 
     try:
-        with smtplib.SMTP(host=os.getenv('EMAIL_HOST'), port=os.getenv('EMAIL_PORT'), timeout=10) as server:  # Added timeout
+        with smtplib.SMTP(
+            host=os.getenv("EMAIL_HOST"), port=os.getenv("EMAIL_PORT"), timeout=10
+        ) as server:  # Added timeout
             server.starttls()  # Secure the connection
-            server.login(os.getenv('EMAIL_HOST_USER'), os.getenv('EMAIL_HOST_PASSWORD'))
+            server.login(os.getenv("EMAIL_HOST_USER"), os.getenv("EMAIL_HOST_PASSWORD"))
             server.send_message(message)
             print("Email has been sent!")
     except smtplib.SMTPException as e:
         print(f"SMTP error occurred: {e}")
     except Exception as e:
         print(f"An error occurred: {e}")
-
--- src/resilience_test.py
+++ src/resilience_test.py
@@ -1,7 +1,6 @@
 from kubernetes import client, config
 from mailer import send_email, get_solution
 from handle_kube_logs import fetch_pod_logs
-import config as local_config
 import time
 import os
 from dotenv import load_dotenv
@@ -26,16 +25,16 @@ def resilience_test(namespace):
     stop_all_pods(namespace)
 
     print("Waiting for the system to stabilize...")
-  
+
     time.sleep(30)  # Wait a minute to allow for pod recreation and system stabilization
 
     errors = fetch_pod_logs(namespace)
     if errors:
         mes = "Errors detected in Kubernetes logs during resilience testing❗❗❗:\n"
-        error_message=mes+"\n"+errors
+        error_message = mes + "\n" + errors
         # print(error_message)
         detailed_solutions = get_solution(error_message)
-        full_message = error_message + "\n\n" + "\n"+detailed_solutions
+        full_message = error_message + "\n\n" + "\n" + detailed_solutions
         send_email("Resilience Test Failure", full_message)
     else:
         send_email(
@@ -43,5 +42,6 @@ def resilience_test(namespace):
             "All pods were stopped and then recovered successfully.",
         )
 
+
 if __name__ == "__main__":
-    resilience_test(os.getenv('NAMESPACE'))
+    resilience_test(os.getenv("NAMESPACE"))
